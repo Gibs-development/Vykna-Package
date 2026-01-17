80e5cdcab350f953f394236ead33aa46684b9796
Gibs-development <cg.lizun@gmail.com>
2026-01-17 14:06:03 +0000

    Guard spot animation access (#12)

--- Vykna Gameserver/src/io/xeros/content/bossfactory/bosses/GoblinNecromancerBoss.java
+++ Vykna Gameserver/src/io/xeros/content/bossfactory/bosses/GoblinNecromancerBoss.java
@@ -30,7 +30,7 @@ public class GoblinNecromancerBoss implements BossController, BossMechanicToggle
     public static final int NPC_ID = 3515;
     public static final int ENTRY_OBJECT_ID = 42344;
     public static final Position TELEPORT_POSITION = new Position(1636, 4821, 0);
-    public static final Position BOSS_SPAWN = new Position(1625, 4830, 0);
+    public static final Position BOSS_SPAWN = new Position(1626, 4816, 0);
     public static final Boundary ARENA_BOUNDARY = new Boundary(1610, 4810, 1649, 4847);
 
     private static final int MAGIC_ANIM = 9829;
