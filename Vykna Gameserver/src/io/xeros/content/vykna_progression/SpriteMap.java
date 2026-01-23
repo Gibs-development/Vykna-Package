bf549fe5e8a90873173a284c0676f9d40bde4627
Gibs-development <cg.lizun@gmail.com>
2026-01-23 20:38:45 +0000

    Began adding correct sprite indexes.

--- Vykna Gameserver/src/io/xeros/content/vykna_progression/SpriteMap.java
+++ Vykna Gameserver/src/io/xeros/content/vykna_progression/SpriteMap.java
@@ -5,35 +5,35 @@ import java.util.stream.Stream;
 public enum SpriteMap {
     ATTACK(1),
     HITPOINTS(2),
-    FIREMAKING(2),
-    WOODCUTTING(2),
-    RUNECRAFTING(2),
-    CRAFTING(2),
-    FARMING(2),
-    SPADE(2),
-    RAT(2),
-    HUNTER(2),
-    STRENGTH(2),
-    FLETCHING(2),
-    DEFENCE(2),
-    RANGED(2),
-    AGILITY(2),
-    PRAYER(2),
-    MAGIC(2),
-    MINING(2),
-    KBD(2),
-    SIRE(2),
-    DUKE(2),
-    BLOODHOUND(2),
-    GRAARDOR(2),
-    SMITHING(2),
-    HYDRA(2),
-    KRIL(2),
-    KREE(2),
-    HERBLORE(2),
-    THIEVING(2),
-    FISHING(2),
-    COOKING(2),
+    FIREMAKING(3),
+    WOODCUTTING(4),
+    RUNECRAFTING(5),
+    CRAFTING(6),
+    FARMING(7),
+    SPADE(8),
+    RAT(9),
+    HUNTER(10),
+    STRENGTH(11),
+    FLETCHING(12),
+    DEFENCE(13),
+    RANGED(14),
+    AGILITY(15),
+    PRAYER(16),
+    MAGIC(17),
+    MINING(18),
+    KBD(19),
+    SIRE(20),
+    DUKE(21),
+    BLOODHOUND(22),
+    GRAARDOR(23),
+    SMITHING(24),
+    HYDRA(25),
+    KRIL(26),
+    KREE(27),
+    HERBLORE(28),
+    THIEVING(29),
+    FISHING(30),
+    COOKING(31),
     SLAYER(32);
 
 
@@ -52,6 +52,8 @@ public enum SpriteMap {
     public static int length() {
         return values().length;
     }
-
+    public int getId() {
+        return id;
+    }
     private final int id;
 }
